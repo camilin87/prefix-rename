21f9b681d39df05e3bc862f3036d1055eeb21185
camilin87 <camilin87@gmail.com>
2016-10-07 10:07:34 -0400

    use the changes calculator in the main

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ require("global-console-prefix")("[PREFIX_RENAME]");
 const rfr = require("rfr")
 const parser = rfr("lib/argsParser")()
 const mapper = rfr("lib/directoryMapper")()
+const changesCalculator = rfr("lib/changesCalculator")()
 
 console.log("renaming files...")
 
@@ -13,9 +14,18 @@ parser.parse().then(
         mapper.getFiles(options.dir).then(
             files => {
 
-                console.log("Files")
-                files.forEach(f => {
-                    console.log(f)
+                // console.log("Files")
+                // files.forEach(f => {
+                //     console.log(f)
+                // })
+
+                var renameOperations = changesCalculator.getChanges(
+                    options.prefix, options.rename, files
+                )
+
+                console.log("Operations")
+                renameOperations.forEach(o => {
+                    console.log(o)
                 })
 
             }, err => {
