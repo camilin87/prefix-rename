ebf3f4961dda2882e0df39664743dc94f6fe27b6
Camilo Sanchez <camilin87@gmail.com>
2016-10-07 16:25:59 -0400

    automate the int test execution

--- spec/generateSample.sh
+++ spec/generateSample.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-rm -R -f ../tmp
-mkdir ../tmp
-touch ../tmp/f1.txt
-touch ../tmp/f2.txt
-touch ../tmp/a1.txt
-touch ../tmp/a2.txt
+rm -R -f ./tmp
+mkdir ./tmp
+touch ./tmp/f1.txt
+touch ./tmp/f2.txt
+touch ./tmp/a1.txt
+touch ./tmp/a2.txt
 
-npm start -- -d ../tmp -p f -r z
+npm start -- -d ./tmp -p f -r z
+
+rm -R -f ./tmp
