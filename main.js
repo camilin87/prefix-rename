09123b1f1b930b97be9f717e9d9ab05aa790350e
camilin87 <camilin87@gmail.com>
2016-10-07 11:06:36 -0400

    implement the big file mover that moves everybody

--- main.js
+++ main.js
@@ -5,33 +5,32 @@ const parser = rfr("lib/argsParser")()
 const mapper = rfr("lib/directoryMapper")()
 const changesCalculator = rfr("lib/changesCalculator")()
 const singleFileMoverFactory = rfr("lib/singleFileMoverFactory")()
+const fileMoverFactory = rfr("lib/fileMover")
 
 console.log("renaming files...")
 
 parser.parse().then(
     options => {
-        console.log(options)
+        console.log("Arguments", options)
 
         var singleFileMover = singleFileMoverFactory.create(options.debug)
+        var fileMover = fileMoverFactory(singleFileMover)
 
         mapper.getFiles(options.dir).then(
             files => {
-
-                // console.log("Files")
-                // files.forEach(f => {
-                //     console.log(f)
-                // })
 
                 var renameOperations = changesCalculator.getChanges(
                     options.prefix, options.rename, files
                 )
 
-                // console.log("Operations")
-                renameOperations.forEach(o => {
-                    // console.log(o)
-                    singleFileMover
-                        .move(options.dir, o.input, o.output)
-                })
+                fileMover
+                    .move(options.dir, renameOperations)
+                    .then(() => {
+                        console.log("files renamed successfully")
+                    }, err => {
+                        console.error(err, err.stack)
+                        process.exit(1)
+                    })
 
             }, err => {
                 console.error(err, err.stack)
