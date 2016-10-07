ba09962788002863621744f7e6b9487ade530871
camilin87 <camilin87@gmail.com>
2016-10-07 11:27:20 -0400

    create an async program

--- main.js
+++ main.js
@@ -1,42 +1,21 @@
 require("global-console-prefix")("[PREFIX_RENAME]");
 
 const rfr = require("rfr")
-const parser = rfr("lib/argsParser")()
-const mapper = rfr("lib/directoryMapper")()
-const changesCalculator = rfr("lib/changesCalculator")()
-const singleFileMoverFactory = rfr("lib/singleFileMoverFactory")()
-const fileMoverFactory = rfr("lib/fileMover")
+const program = rfr("lib/program")(
+    rfr("lib/argsParser")(),
+    rfr("lib/singleFileMoverFactory")(),
+    rfr("lib/fileMover"),
+    rfr("lib/changesCalculator")(),
+    rfr("lib/directoryMapper")()
+)
 
 console.log("renaming files...")
 
-parser.parse().then(
-    options => {
-        console.log("Arguments", options)
-
-        var singleFileMover = singleFileMoverFactory.create(options.debug)
-        var fileMover = fileMoverFactory(singleFileMover)
-
-        mapper.getFiles(options.dir).then(
-            files => {
-
-                var renameOperations = changesCalculator.getChanges(
-                    options.prefix, options.rename, files
-                )
-
-                fileMover
-                    .move(options.dir, renameOperations)
-                    .then(() => {
-                        console.log("files renamed successfully")
-                    }, err => {
-                        console.error(err, err.stack)
-                        process.exit(1)
-                    })
-
-            }, err => {
-                console.error(err, err.stack)
-                process.exit(1)
-            }
-        )
+program
+    .run()
+    .then(() => {
+        console.log("files renamed successfully")
+        process.exit(0)
     }, err => {
         console.error(err, err.stack)
         process.exit(1)
