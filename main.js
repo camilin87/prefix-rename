30f316c25acc66c4b52fb7d027443866d706f12c
camilin87 <camilin87@gmail.com>
2016-10-07 08:29:02 -0400

    rename the internal parameter names

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ console.log("renaming files...")
 const commandLineArgs = require("command-line-args")
 
 const optionDefinitions = [
-    { name: "dir", alias: 'd', type: String, multiple: true },
-    { name: "prefix", alias: 'p', type: String, multiple: true }
+    { name: "dirs", alias: 'd', type: String, multiple: true },
+    { name: "prefixes", alias: 'p', type: String, multiple: true }
 ]
 
 const options = commandLineArgs(optionDefinitions)
