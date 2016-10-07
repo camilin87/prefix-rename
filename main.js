e5d1d0740fb5a4fa8e28f43a9c6856e2b07b73b1
camilin87 <camilin87@gmail.com>
2016-10-07 08:23:43 -0400

    parse the command line arguments

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 require("global-console-prefix")("[PREFIX_RENAME]");
 
 console.log("renaming files...")
+
+const commandLineArgs = require("command-line-args")
+
+const optionDefinitions = [
+    { name: "dir", alias: 'd', type: String, multiple: true },
+    { name: "prefix", alias: 'p', type: String, multiple: true }
+]
+
+const options = commandLineArgs(optionDefinitions)
+
+console.log(options)
